f5c349c39b2ab632f9e1b91ac73262ce4793d84e
Gabriel Guimaraes <gabrielplug32@gmail.com>
2024-09-15 13:23:26 -0300

    [200~starting project

---
+++ src/main/java/com/mycompany/trabalhoprog/ii/Application.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhoprog.ii;
+import java.time.LocalDate;
+/**
+ *
+ * @author Gabriel Amorim Guimarães
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        LocalDate data =  LocalDate.now();
+        System.out.println("Mostrar a data atual: "+data);
+    }
+}
---
+++ src/main/java/com/mycompany/trabalhoprog/ii/Model/ProdutoModel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalhoprog.ii.Model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public abstract class ProdutoModel {
+           private String nome;
+           private double preco;
+           private double qtdEstoque;
+           
+           
+}
